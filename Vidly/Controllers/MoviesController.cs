8eda4f95ba132b17665fa7071dadb6aa265141cf
Ivo Yueh <bitxwise@gmail.com>
2016-12-23 23:22:06 -0700

    Added basic movie functionality

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() {
+                Name = "Shrek!"
+            };
+
+            return View(movie);
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}")]
+        public ActionResult ByReleaseYear(int year)
+        {
+            return Content(year + " Releases");
+        }
+    }
+}
