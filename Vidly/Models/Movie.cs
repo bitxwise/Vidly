a61357abee2b183b1179f54cf4c0e4c122c5748b
Ivo Yueh <bitxwise@gmail.com>
2016-12-25 01:39:17 -0700

    Added dates and stock to movie

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -11,5 +11,8 @@ namespace Vidly.Models
         public string Name { get; set; }
         public Genre Genre { get; set; }
         public short? GenreId { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime AddedDate { get; set; }
+        public int NumberInStock { get; set; }
     }
 }
