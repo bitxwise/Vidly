d847e5da128736e70d34dd9e00032b1fb413ec78
Ivo Yueh <bitxwise@gmail.com>
2016-12-25 22:50:37 -0700

    Added customer form to create and edit customers

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -40,6 +41,52 @@ namespace Vidly.Controllers
                 return HttpNotFound();
 
             return View(customer);
+        }
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel() {
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.Single(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel() {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if(customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                // TODO: Replace individual property setting with mapping (e.g. AutoMapper)
+                var existingCustomer = _context.Customers.Single(c => c.Id == customer.Id);
+                existingCustomer.BirthDate = customer.BirthDate;
+                existingCustomer.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+                existingCustomer.MembershipTypeId = customer.MembershipTypeId;
+                existingCustomer.Name = customer.Name;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
         }
     }
 }
